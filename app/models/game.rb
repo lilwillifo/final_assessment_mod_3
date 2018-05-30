04005ee8e6b573a1bac402c4bb8bc74be9bc43df
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-30 11:04:21 -0600

    moving scores out of serializer

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,21 @@ class Game < ApplicationRecord
   belongs_to :player_2, class_name: "User", foreign_key: :player_2_id
 
   has_many :plays
+
+  def scores
+    [{'user_id': self.player_1_id ,
+      'score': sum_scores(self.player_1_id),
+    },
+    {'user_id': self.player_2_id ,
+      'score': sum_scores(self.player_2_id),
+    }]
+  end
+
+  private
+  def sum_scores(player_id)
+    player = User.find(player_id)
+    player.plays.map do |play|
+      play.score
+    end.sum
+  end
 end
